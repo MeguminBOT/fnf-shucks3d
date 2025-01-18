177cb4a1981c465536980edffd7895a7258f7beb
AutisticLulu <discord.meguminbot@gmail.com>
2025-01-18 14:17:48 +0100

    No longer drains Health during GF singing

--- example_mods/scripts/Health Drain.lua
+++ example_mods/scripts/Health Drain.lua
@@ -1,7 +1,11 @@
-function opponentNoteHit()
+function opponentNoteHit(id, direction, noteType, isSustainNote)
 	local curHealth = getProperty('health')
 	local healthThreshold = 0.5
 	local healthDecrease = 0.007
+	
+	if gfSection and not mustHitSection or mustHitSection and noteType == 'GF Sing' then
+		return;
+	end
 
 	if curHealth > healthThreshold then
 		setProperty('health', curHealth - healthDecrease)
